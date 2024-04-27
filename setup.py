2b8253be35ddd4c90788e375cc6eefbfeec62912
Spencer Kinney <spencer_kinney@yahoo.com>
2024-04-27 15:09:26 -0500

    duh BAM setup

--- setup.py
+++ setup.py
@@ -1,33 +1,38 @@
-# setup.py
+import os
 from setuptools import setup, find_packages
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
-    name='bambot',
-    version='0.3.0',
-    author='Bam Corp.',
-    author_email='spencer@bam.bot',
-    description='A Python package for interfacing with Bam data storage.',
-    long_description=open('README.md').read(),
-    long_description_content_type='text/markdown',
-    url='https://github.com/BamCorp/bambot',
+    name="bambot",
+    version="0.4.0",
+    author="Bam Corp",
+    author_email="spencer@bam.bot",
+    description="A framework for deploying AI agents as Docker containers",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/BamCorp/bambot",
     packages=find_packages(),
     install_requires=[
-        "requests",
-        "tqdm"
+        "click",
+        "jinja2",
+        "tqdm",
     ],
+    entry_points={
+        "console_scripts": [
+            "bam=bambot.cli:cli",
+        ],
+    },
     classifiers=[
-        'Development Status :: 4 - Beta',
-        'Intended Audience :: Developers',
-        'Topic :: Software Development :: Libraries :: Python Modules',
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-        'Programming Language :: Python :: 3.10',
-        'Programming Language :: Python :: 3.11',
-        'Programming Language :: Python :: 3.12',
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
     ],
-    python_requires='>=3.6, <4',
+    python_requires=">=3.6",
 )
