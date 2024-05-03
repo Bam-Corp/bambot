50bd343a7cb08eac9794a7bb98cb825733453f86
Spencer Kinney <spencer_kinney@yahoo.com>
2024-05-02 19:30:35 -0500

    modified signatures for docker utils

--- bambot/docker_utils.py
+++ bambot/docker_utils.py
@@ -1,4 +1,4 @@
-# bam/docker_utils.py
+# docker_utils.py
 import os
 import docker
 from tqdm import tqdm
@@ -14,26 +14,38 @@ def create_docker_container(container_name, env_vars=None):
     docker_client = docker.from_env()
     container = docker_client.containers.create(
         f"{container_name}:latest",
-        detach=True,
         name=container_name,
-        environment=env_vars
+        environment=env_vars,
+        detach=True,
+        tty=True,
+        stdin_open=True,
+        ports={'1337/tcp': ('127.0.0.1', 1337)},
     )
     return container
 
-def prune_system():
+def prune_system(progress_bar=None):
     """Prune Docker system resources"""
     docker_client = docker.from_env()
-    with tqdm(total=4, desc="Pruning system resources") as pbar:
-        docker_client.containers.prune()
-        pbar.update(1)
-        docker_client.images.prune()
-        pbar.update(1)
-        docker_client.networks.prune()
-        pbar.update(1)
-        docker_client.volumes.prune()
-        pbar.update(1)
+    tasks = [
+        docker_client.containers.prune,
+        docker_client.images.prune,
+        docker_client.networks.prune,
+        docker_client.volumes.prune,
+    ]
+    if progress_bar:
+        for task in tasks:
+            task()
+            progress_bar.update(1)
+    else:
+        with tqdm(total=4, desc="Pruning system resources") as pbar:
+            for task in tasks:
+                task()
+                pbar.update(1)
 
-def list_containers():
-    """List all Docker containers"""
+def list_apps(prefix=""):
+    """List all Bam apps"""
     docker_client = docker.from_env()
-    return docker_client.containers.list(all=True)
+    containers = docker_client.containers.list(all=True)
+    if prefix:
+        containers = [c for c in containers if c.name.startswith(prefix)]
+    return containers
