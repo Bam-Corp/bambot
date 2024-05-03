e61f6d3c92a09b67d6e0af3920e17b0a37c386b1
Spencer Kinney <spencer_kinney@yahoo.com>
2024-05-02 19:30:55 -0500

    removing unused metrics and logging

--- bambot/logging.py
+++
@@ -1,5 +0,0 @@
-import logging
-
-def setup_logging():
-    """Set up logging for the Bam CLI application"""
-    logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(name)s - %(levelname)s - %(message)s")
